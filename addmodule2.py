1166500270cab03d376f9c983da13c9858254b4c
drozdov71 <41123200+drozdov71@users.noreply.github.com>
2018-07-24 13:39:47 +0300

    week 3

---
+++ addmodule2.py
@@ -0,0 +1,32 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+'''
+Имеется набор файлов, каждый из которых, кроме последнего, содержит имя следующего файла.
+Первое слово в тексте последнего файла: "We".
+
+Скачайте предложенный файл. В нём содержится ссылка на первый файл из этого набора.
+
+Все файлы располагаются в каталоге по адресу:
+https://stepic.org/media/attachments/course67/3.6.3/
+
+Загрузите содержимое ﻿последнего файла из набора, как ответ на это задание.
+'''
+import requests
+with open('C:/Users/Dmitry/Downloads/dataset_3378_3.txt', 'r') as f:
+    url = f.readline().strip()
+folder = 'https://stepic.org/media/attachments/course67/3.6.3/'
+text = ''
+while text[:2] != 'We':
+    r = requests.get(url)
+    text = r.text
+    url = folder + text
+    print(text)
+print('--------------')
+print('Thats all. Check "C:/Users/Dmitry/Downloads/dataset_3378_3.txt')
+with open('C:/Users/Dmitry/Downloads/dataset_3378_3.txt', 'w') as f:
+    f.write(text)
+
